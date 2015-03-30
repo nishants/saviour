d279a4e817543a511cdac2b9e89d1219b6af145f
nsing <nishant.singh87@gmail.com>
2015-03-30 09:05:28 +0000

    adding some style to search box, adding ngTouch lib

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,5 +1,5 @@
 (function() {
   'use strict';
   window.saviour = {};
-  window.saviour.app = angular.module("saviour", ['ui.router']);
+  window.saviour.app = angular.module("saviour", ['ui.router', 'ngTouch']);
 }).call(this);
--- app/scripts/controllers/app-controller.js
+++ app/scripts/controllers/app-controller.js
@@ -1,8 +1,13 @@
 (function() {
     'use strict';
-    saviour.app.controller('AppController', function($scope) {
-      $scope.showInsructions = function(){
+    saviour.app.controller('AppController', function($scope, $location) {
+      $scope.loadLocations = function(){
         alert("Enter something");
+      };
+      console.log("hello");
+      $scope.searchPlace = function(){
+        console.log("I am touched...");
+        $location.url("search");
       };
     });
 }).call(this);
