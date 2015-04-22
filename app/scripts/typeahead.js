813d276637ce0c082f2a16eeb40302048042fbb7
nsing <nishant.singh87@gmail.com>
2015-04-22 07:57:53 +0000

    adding keywords.json to stub backend

--- app/scripts/typeahead.js
+++ app/scripts/typeahead.js
@@ -3,21 +3,21 @@
   
   var SearchKeywords = function(values){
   	this.keywords = values;
-  	this.$elementId = "#search-keywords";
+  	this.elementId = "#search-keywords";
   };
   
-  SearchKeywords.prototype.$addOptions = function(){
+  SearchKeywords.prototype.$add = function(){
   	var keywords = this.keywords;
   	var optionFor = function(keyword){
   		return $("<option>", {value: keyword});
   	};
   	for(var i = 0; i < keywords.length; i++){
-  		$(this.$elementId).append(optionFor(keywords[i]));
+  		$(this.elementId).append(optionFor(keywords[i]));
   	}
   };
   
   SearchKeywords.add = function(values){
-  	new SearchKeywords(values).$addOptions();
+  	new SearchKeywords(values).$add();
   };
   
   window.saviour.SearchKeywords = SearchKeywords;
